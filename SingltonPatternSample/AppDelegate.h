a5ae7b93118ccc4b696415212671a536b34134b1
chengkaizone <chengkaizone@163.com>
2015-10-11 11:12:27 +0800

    Initial Commit

---
+++ SingltonPatternSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingltonPatternSample
+//
+//  Created by lance on 15/10/11.
+//  Copyright © 2015年 lance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
